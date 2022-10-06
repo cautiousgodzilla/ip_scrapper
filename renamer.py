8825e27b6a142bcd830e8b2b8d730e73ba3a3429
cautiousgodzilla <92043508+cautiousgodzilla@users.noreply.github.com>
2022-10-06 23:28:43 +0530

    Add files via upload

---
+++ renamer.py
@@ -0,0 +1,11 @@
+import glob
+import os
+
+dr = "D:\Projects\scraps\IPO\ipo_data"
+for i in glob.glob(os.path.join(dr,"*.pdf")):
+    path = i.split('\\')
+    name = path[-1].split('_')
+    #print(name)
+    rename_ = 'D:\\Projects\\scraps\\IPO\\ipo_data\\ '.strip()+ name[1][:4] + '_' + name[0] + ".pdf"
+    print(rename_)
+    os.rename(i, rename_)
